f939c931230affa2c0e34da3a2bf135cc6bca2f5
Furse <Fursevich.dasha@mail.ru>
2023-07-24 15:00:40 +0300

    ss

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int n1=number/1000;
+        int n2=(number-n1*1000)/100;
+        int n3=(number-n1*1000-n2*100)/10;
+        int n4=number-n1*1000-n2*100-n3*10;
+        int sum=n1+n2+n3+n4;
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int n1=number/100;
+        int n2=(number-n1*100)/10;
+        int n3=number-n1*100-n2*10;
+        System.out.print(n3);
+        System.out.print(n2);
+        System.out.print(n1);
+        System.out.print('\n');
+
     }
 }
